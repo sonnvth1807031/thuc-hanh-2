c77316260b3ff7f630da1a6a27ba07d2c3978d32
sonnvth1807031 <sonnvth1807031@fpt.edu.vn>
2019-04-08 20:50:43 +0700

    first commit

---
+++ thuchanh2/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+import entity.Flight;
+public class Main {
+
+    public static void main(String[] args) {
+        Flight gl = new Flight(857,"Toronto");
+        gl.display();
+    }
+}
